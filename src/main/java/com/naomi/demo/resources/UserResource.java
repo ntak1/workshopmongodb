38a1ef54890aabc89a1240f916ea1107affbee4b
Naomi Takemoto <naomitkm@amazon.com>
2020-08-25 20:50:37 -0300

    Using DTO to return users

--- src/main/java/com/naomi/demo/resources/UserResource.java
+++ src/main/java/com/naomi/demo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.naomi.demo.resources;
 
 import com.naomi.demo.domain.User;
+import com.naomi.demo.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.naomi.demo.services.UserService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,9 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
