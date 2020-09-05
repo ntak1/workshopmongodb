8ab516cf35e15270e5151a18135310ea96234ed4
Naomi Takemoto <naomitkm@amazon.com>
2020-09-05 19:21:47 -0300

    reference user posts

--- src/main/java/com/naomi/demo/config/Instantiation.java
+++ src/main/java/com/naomi/demo/config/Instantiation.java
@@ -43,5 +43,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("23/03/2018"),"Partiu férias", "Vou viajar para RJ.",
                 new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
